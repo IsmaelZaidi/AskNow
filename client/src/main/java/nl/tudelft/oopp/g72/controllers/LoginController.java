875706610d0f36d79f705b2d35f55e4000ded150
Jannick Weitzel <j.a.g.weitzel@student.tudelft.nl>
2021-03-20 15:21:33 +0100

    All values initionalized and controller set up

--- client/src/main/java/nl/tudelft/oopp/g72/controllers/LoginController.java
+++ client/src/main/java/nl/tudelft/oopp/g72/controllers/LoginController.java
@@ -1,4 +1,22 @@
 package nl.tudelft.oopp.g72.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField displayName;
+    @FXML
+    private TextField roomCode;
+
+    public void joinRoom() {
+        // Will be executed when 'join' button is clicked.
+        System.out.println("Displayname: " + displayName.getText());
+        System.out.println("Room code: " + roomCode.getText());
+    }
+
+    public void createRoom() {
+        // Will be executed when 'create room' button is clicked.
+    }
 }
