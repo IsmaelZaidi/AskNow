0a4ffc71c566fb8a6aa989e2a272ba83e7ce96f9
Jannick Weitzel <j.a.g.weitzel@student.tudelft.nl>
2021-03-21 21:32:18 +0100

    Added javadoc to login controller + general fixes

--- client/src/main/java/nl/tudelft/oopp/g72/controllers/LoginController.java
+++ client/src/main/java/nl/tudelft/oopp/g72/controllers/LoginController.java
@@ -5,17 +5,24 @@ import javafx.scene.control.TextField;
 
 public class LoginController {
 
-    @FXML
+    @FXML // Value in display name input field.
     private TextField displayName;
-    @FXML
+    @FXML // Value in room code input field.
     private TextField roomCode;
 
+
+    /**
+     * Executed when the 'join room' button is clicked.
+     */
     public void joinRoom() {
         // Will be executed when 'join' button is clicked.
         System.out.println("Displayname: " + displayName.getText());
         System.out.println("Room code: " + roomCode.getText());
     }
 
+    /**
+     * Executed when the 'create room' button is clicked.
+     */
     public void createRoom() {
         // Will be executed when 'create room' button is clicked.
     }
