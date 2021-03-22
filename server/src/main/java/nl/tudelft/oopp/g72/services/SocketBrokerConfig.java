a5ed885d0f2128d78b049e0bf52f0ce592515322
CristianCutitei <c.cutitei@student.tudelft.nl>
2021-03-22 09:22:42 -0700

    dependencies

---
+++ server/src/main/java/nl/tudelft/oopp/g72/services/SocketBrokerConfig.java
@@ -0,0 +1,17 @@
+package nl.tudelft.oopp.g72.services;
+
+public class SocketBrokerConfig extends
+        AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/secured/user/queue/specific-user");
+        config.setApplicationDestinationPrefixes("/spring-security-mvc-socket");
+        config.setUserDestinationPrefix("/secured/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/secured/room").withSockJS();
+    }
+}
