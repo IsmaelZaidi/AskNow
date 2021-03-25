f6e568bd031f09c9e48b45601e168f980841a6d5
Jannick Weitzel <j.a.g.weitzel@student.tudelft.nl>
2021-03-25 22:27:21 +0100

    Set up assistant view

--- client/src/main/java/nl/tudelft/oopp/g72/controllers/AssistantController.java
+++ client/src/main/java/nl/tudelft/oopp/g72/controllers/AssistantController.java
@@ -1,4 +1,52 @@
 package nl.tudelft.oopp.g72.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class AssistantController {
+
+    @FXML
+    private Label studentCount;
+
+    /**
+     * Executed when 'info' button is clicked.
+     */
+    public void info() {
+
+    }
+
+    /**
+     * Executed when 'merge' button is clicked.
+     */
+    public void merge() {
+
+    }
+
+    /**
+     * Executed when 'edit' button is clicked.
+     */
+    public void edit() {
+
+    }
+
+    /**
+     * Executed when 'remove' button is clicked.
+     */
+    public void remove() {
+
+    }
+
+    /**
+     * Executed when 'teacher's view' button is clicked.
+     */
+    public void teacherView() {
+
+    }
+
+    /**
+     * Executed when 'quit' button is clicked.
+     */
+    public void quit() {
+
+    }
 }
