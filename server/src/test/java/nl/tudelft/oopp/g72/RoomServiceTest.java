62e741e685056c4209b3e85d1db786350280a6a7
Ismael Zaidi <i.zaidi@student.tudelft.nl>
2021-04-09 12:25:57 +0200

    checkstyle

--- server/src/test/java/nl/tudelft/oopp/g72/RoomServiceTest.java
+++ server/src/test/java/nl/tudelft/oopp/g72/RoomServiceTest.java
@@ -4,19 +4,18 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.time.OffsetDateTime;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import nl.tudelft.oopp.g72.models.Room;
 import nl.tudelft.oopp.g72.models.User;
 import nl.tudelft.oopp.g72.repositories.RoomRepository;
 import nl.tudelft.oopp.g72.repositories.UserRepository;
 import nl.tudelft.oopp.g72.services.RoomService;
 import nl.tudelft.oopp.g72.services.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
-
 
 
 @SpringBootTest
